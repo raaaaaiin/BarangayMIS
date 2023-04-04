5a17434d3b4a2054743a7ae84529aa901f93478e
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2023-04-05 01:29:16 +0800

    Added Navbar on login

--- index.php
+++ index.php
@@ -4,36 +4,62 @@
     <title>Barangay Management System</title>
   </head>
   <body>
+    <table width="100%">
+      <tr>
+        <td>
+          <a href="#">Home</a>
+          <a href="#">Contact</a>
+          <a href="#">About</a>
+        </td>
+        <td align="right">
+          <a href="#">Log in</a>
+          <a href="#">Register</a>
+        </td>
+      </tr>
+    </table>
+    
     <h1>Welcome to Barangay Management System</h1>
-    <p>This system is designed to help manage the activities and information of our barangay. Please login to access the system.</p>
-    <form action="login.php" method="post">
-      <label for="username">Username:</label>
-      <input type="text" name="username" id="username"><br>
-      <label for="password">Password:</label>
-      <input type="password" name="password" id="password"><br>
-      <input type="submit" value="Login">
-    </form>
     
-    <h2>Latest News and Updates</h2>
-    <?php
-      // Include the database configuration
-      include('db.php');
-
-      // Retrieve the latest news
-      $query = "SELECT * FROM news_events WHERE type='news' ORDER BY date DESC LIMIT 5";
-      $result = mysqli_query($conn, $query);
-
-      // Display the news as a list
-      echo "<ul>";
-      while ($row = mysqli_fetch_assoc($result)) {
-        echo "<li><strong>" . $row['title'] . "</strong> - " . $row['description'] . "</li>";
-      }
-      echo "</ul>";
-
-      // Close the database connection
-      mysqli_close($conn);
-    ?>
+    <section>
+      <h2>About Sitio Igiban, Barangay Sta. Cruz, Antipolo City, Rizal</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rutrum mauris vitae eros tincidunt malesuada. Donec auctor arcu ac nisl eleifend, at imperdiet odio cursus. Sed vel purus sit amet ex ultricies aliquet. </p>
+      <img src="http://via.placeholder.com/250" alt="Barangay Hall">
+    </section>
     
-    <p>Note: The latest news is updated every few days.</p>
+    <section>
+      <h2>Barangay Projects</h2>
+      <ul>
+        <li>Construction of new barangay hall</li>
+        <li>Installation of new street lights</li>
+        <li>Launching of new waste management program</li>
+        <li>Upgrading of barangay health center facilities</li>
+        <li>Repair of damaged roads and sidewalks</li>
+        <li>Construction of new basketball court</li>
+      </ul>
+      <img src="http://via.placeholder.com/250" alt="Barangay Projects">
+    </section>
+    
+    <section>
+      <h2>Latest News and Updates</h2>
+      <?php
+        // Include the database configuration
+        include('db.php');
+
+        // Retrieve the latest news
+        $query = "SELECT * FROM news_events";
+        $result = mysqli_query($conn, $query);
+
+        // Display the news as a list
+        echo "<ul>";
+        while ($row = mysqli_fetch_assoc($result)) {
+          echo "<li><strong>" . $row['title'] . "</strong> - " . $row['description'] . "</li>";
+        }
+        echo "</ul>";
+
+        // Close the database connection
+        mysqli_close($conn);
+      ?>
+    </section>
+    
   </body>
 </html>
